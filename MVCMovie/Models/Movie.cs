cb406e06f6c18c30a6a2b31f83540f7950b443f7
klowey <klowey@W766299>
2017-02-14 19:41:32 -0700

    Initial project to repository

---
+++ MVCMovie/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode= true)]
+
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Required]
+        public Decimal Price { get; set; }      
+
+
+    }
+}
