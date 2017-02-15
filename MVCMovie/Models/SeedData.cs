3becdf06b7d4f24cc665ac1d03caf47448469c78
klowey <klowey@W766299>
2017-02-14 21:02:33 -0700

    Add Seed Data class and methods to populate data

---
+++ MVCMovie/Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCMovie.Data;
+
+namespace MVCMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context =
+                new ApplicationDbContext(
+                    serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+
+            {
+                //look for any records, if there are records do nothing
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+
+                context.Movie.AddRange(
+
+                    new Movie
+                    {
+                        Title = "Lemmy",
+                        ReleaseDate = DateTime.Parse("2001-01-01"),
+                        Genre = "Documentary",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Secret Life of Birds",
+                        ReleaseDate = DateTime.Parse("2001-01-01"),
+                        Genre = "Documentary",
+                        Price = 9.99M
+
+                    },
+                    new Movie
+                    {
+                        Title = "Man Bites Dog",
+                        ReleaseDate = DateTime.Parse("2001-01-01"),
+                        Genre = "Foreign",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Repo Man",
+                        ReleaseDate = DateTime.Parse("2001-01-01"),
+                        Genre = "Cult Classic",
+                        Price = 9.99M
+
+                    }
+
+                    );
+                context.SaveChanges();
+            }
+
+        }
+
+
+    }
+}
--- MVCMovie/Startup.cs
+++ MVCMovie/Startup.cs
@@ -81,8 +81,10 @@ namespace MVCMovie
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Movies}/{action=Index}/{id?}");
             });
+
+            SeedData.Initialize(app.ApplicationServices);
         }
     }
 }
